0a9b161a05fed27f3ce09146c7b192c198b7a347
Spikxzy <99260885+Spikxzy@users.noreply.github.com>
2022-02-22 08:47:55 +0100

    Add project files.

---
+++ COMEarlyBinding/ICOMObject.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace COMEarlyBinding
+{
+    [ComVisible(true)]
+    [Guid(AssemblyInfo.InterfaceGuid)]
+    [InterfaceType(ComInterfaceType.InterfaceIsDual)]
+    public interface ICOMObject
+    {
+        double Addition(double firstValue, double secondValue);
+        string HelloWorld();
+    }
+}
