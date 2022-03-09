29891ed08b643346188f67ff06ead7f6a8f8d4a6
Spikxzy <michael.rumpelnig@omicronenergy.com>
2022-03-09 11:22:14 +0100

    new COM object with managed code.

---
+++ COM-with-managed-code/ILessSimpleComObject.cs
@@ -0,0 +1,14 @@
+﻿namespace COM_obj_with_events
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    [ComImport]
+    [Guid(AssemblyInfo.ComObjInterfaceGuid)]
+    [InterfaceType(ComInterfaceType.InterfaceIsDual)]
+    public interface ILessSimpleComObject
+    {
+        IResult Addition(double firstValue, double secondValue);
+        string HelloWorld();
+    }
+}
---
+++ COM-with-managed-code/LessSimpleComObject.cs
@@ -0,0 +1,22 @@
+﻿namespace COM_obj_with_events
+{
+    using System;
+    using System.Runtime.InteropServices;
+
+    [ComVisible(true)]
+    [Guid(AssemblyInfo.ComObjClassGuid)]
+    [ClassInterface(ClassInterfaceType.None)]
+    [ProgId("LessSimpleComObject")]
+    public class LessSimpleComObject : ILessSimpleComObject
+    {
+        public IResult Addition(double firstValue, double secondValue)
+        {
+            return new Result(firstValue + secondValue);
+        }
+
+        public string HelloWorld()
+        {
+            return "Hello World!";
+        }
+    }
+}
